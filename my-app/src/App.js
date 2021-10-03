c98522857c0d09627498c2b63c3b6a61bc8667d4
= <=>
2021-10-03 14:36:28 -0500

    Cambios fallidos (NO CONTINUAR DESDE ACÁ)
    Edición erronea sobre el codigo, REACT no se ejecuta.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,23 @@
-
+import logo from './logo.svg';
 import './App.css';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <h1> ¡Bienvenido a MegaMarket! </h1>
-
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-        <body className="App-body">
-          <h2>¡La tienda de Hardware por excelencia!</h2>
-            
-        </body>
     </div>
   );
 }
